a661398ab13c28bdf1e9b3c69a03ba19d301e869
sweepas <47362063+sweepas@users.noreply.github.com>
2024-01-22 17:42:00 +0000

    new-link

--- src/assets/projects.jsx
+++ src/assets/projects.jsx
@@ -11,7 +11,7 @@ const projects = [
       description:
         "This project is a web App that allows us to access and manipulate data stored in an SQL database served utilising a custom API. User can create an account, add or remove new articles, comments, topics. Vote for favorite comments and topics if signed in. Sort articles by topic, date added or by other users interactions with an article(most voted, comment count).",
       stack: ["React", "JavaScript", "vite", "axios", "CSS", 'netlify'],
-      link: "https://657f1d4558f3e607e195c80e--beautiful-mandazi-c4e8c2.netlify.app/",
+      link: "https://65aea489620604000962887d--beautiful-mandazi-c4e8c2.netlify.app/articles",
     },
     {
       id: "nc-news-api",
